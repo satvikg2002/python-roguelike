5423c9fd6798b5412589d442897240e09b143348
Satvik <satvik.noida@gmail.com>
2024-05-06 20:13:32 +0530

    refactoring

--- input_handlers.py
+++ input_handlers.py
@@ -1,30 +1,54 @@
-from typing import Optional     # give None values to vars
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
 from actions import Action, EscapeAction, BumpAction
 
 
+if TYPE_CHECKING:
+    from engine import Engine
+
+
 # send event to proper method
 class EventHandler(tcod.event.EventDispatch[Action]):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+            self.engine.handle_enemy_turns()
+            self.engine.update_fov()  # Update the FOV before the players next action.
+
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
     
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
+        
         key = event.sym
 
-        if key == tcod.event.KeySym.UP:
-            action = BumpAction(dx=0, dy=-1)
-        elif key == tcod.event.KeySym.DOWN:
-            action = BumpAction(dx=0, dy=1)
-        elif key == tcod.event.KeySym.LEFT:
-            action = BumpAction(dx=-1, dy=0)
-        elif key == tcod.event.KeySym.RIGHT:
-            action = BumpAction(dx=1, dy=0)
+        player = self.engine.player
 
-        elif key == tcod.event.KeySym.ESCAPE:
-            action = EscapeAction()
+        if key == tcod.event.K_UP:
+            action = BumpAction(player, dx=0, dy=-1)
+        elif key == tcod.event.K_DOWN:
+            action = BumpAction(player, dx=0, dy=1)
+        elif key == tcod.event.K_LEFT:
+            action = BumpAction(player, dx=-1, dy=0)
+        elif key == tcod.event.K_RIGHT:
+            action = BumpAction(player, dx=1, dy=0)
+
+        elif key == tcod.event.K_ESCAPE:
+            action = EscapeAction(player)
 
         # no valid keypress
         return action
--- procgen.py
+++ procgen.py
@@ -12,7 +12,7 @@ import tile_types
 
 
 if TYPE_CHECKING:
-    from entity import Entity
+    from engine import Engine
 
 
 class RectangularRoom:
@@ -89,10 +89,11 @@ def generate_dungeon(
         map_width: int,
         map_height: int,
         max_monsters_per_room: int,
-        player: Entity,
+        engine: Engine,
 ) -> GameMap:
     """ Generate a dungeon map """
-    dungeon = GameMap(map_width, map_height, entities=[player])
+    player = engine.player
+    dungeon = GameMap(engine, map_width, map_height, entities=[player])
 
     rooms: List[RectangularRoom] = []
 
@@ -114,7 +115,7 @@ def generate_dungeon(
 
         if len(rooms) == 0:
             # first room
-            player.x, player.y = new_room.center
+            player.place(*new_room.center, dungeon)
         else:
             # connect rooms
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
